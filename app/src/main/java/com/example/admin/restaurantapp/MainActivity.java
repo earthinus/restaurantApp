5c6cabbe31d84b42f8f4f46c1099806fc98f98c7
Mai Hoshino <insomnia.vov@gmail.com>
2016-11-03 12:30:06 -0700

    change progress bar style

--- app/src/main/java/com/example/admin/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/admin/restaurantapp/MainActivity.java
@@ -1,32 +1,18 @@
 package com.example.admin.restaurantapp;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //launch screen
         setContentView(R.layout.index);
+        //launch screen
         Handler handler = new Handler();
-        handler.postDelayed(new jumpPage(), 3000);
+        handler.postDelayed(new jumpPage(), 6000);
 
         // TODO : 3. Show Restaurant Detail (Maki)
         // 1. Load array data by using restaurant's id
@@ -63,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         //
 
     }
-
     //splash
     class jumpPage implements Runnable {
         public void run() {
